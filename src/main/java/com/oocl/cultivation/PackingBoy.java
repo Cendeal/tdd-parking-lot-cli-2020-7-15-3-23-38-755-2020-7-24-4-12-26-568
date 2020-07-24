b9a964d701dc1c74b4834255ed2dc8a6ad476013
Cendeal <5205093You>
2020-07-24 15:35:31 +0800

    [story1][cendeal]test:add should_return_car_when_packing_car_given_car test

---
+++ src/main/java/com/oocl/cultivation/PackingBoy.java
@@ -0,0 +1,10 @@
+package com.oocl.cultivation;
+
+public class PackingBoy {
+    public PackingBoy(PackingLot packingLot) {
+    }
+
+    public PackingTicket parking(Car car) {
+        return null;
+    }
+}
--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -1,10 +1,23 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.PackingBoy;
+import com.oocl.cultivation.PackingLot;
+import com.oocl.cultivation.PackingTicket;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ParkingBoyFacts {
     @Test
-    void write_your_first_test() {
-
+    void should_return_car_when_packing_car_given_car() {
+        //given
+        Car car = new Car();
+        PackingLot packingLot = new PackingLot();
+        PackingBoy packingBoy = new PackingBoy(packingLot);
+        //when
+        PackingTicket ticket = packingBoy.parking(car);
+        //then
+        assertNotNull(ticket);
     }
 }
