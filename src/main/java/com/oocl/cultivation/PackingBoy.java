03caac93a509dc873721d16f15efc40eef902b7c
Cendeal <1798062051@qq.com>
2020-07-26 15:56:58 +0800

    [story3-ac1][cendeal]feature: implement packingboy add packing lot

--- src/main/java/com/oocl/cultivation/PackingBoy.java
+++ src/main/java/com/oocl/cultivation/PackingBoy.java
@@ -5,25 +5,29 @@ import com.oocl.cultivation.exception.NoPositionException;
 import com.oocl.cultivation.exception.ProvideTicketException;
 import com.oocl.cultivation.exception.UnrecognizedException;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class PackingBoy {
-    private final PackingLot packingLot;
+    private final List<PackingLot> packingLots;
 
     public PackingBoy(PackingLot packingLot) {
-        this.packingLot = packingLot;
+        this.packingLots = new LinkedList<>();
+        this.packingLots.add(packingLot);
     }
 
     public PackingTicket parking(Car car) throws NoPositionException {
-        return this.packingLot.packACar(car);
+        return this.packingLots.get(0).packACar(car);
     }
 
     public Car fetch(PackingTicket ticket) throws UnrecognizedException, ProvideTicketException {
         if (ticket == null) {
             throw new ProvideTicketException();
         }
-        return this.packingLot.getCar(ticket);
+        return this.packingLots.get(0).getCar(ticket);
     }
 
     public boolean addPackingLot(PackingLot packingLot2) {
-        return false;
+        return this.packingLots.add(packingLot2);
     }
 }
